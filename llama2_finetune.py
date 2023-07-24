833d3f3411f864587f34fe598012f01aefe7500a
Adrian Theuma <adriantheuma@gmail.com>
2023-07-24 21:25:42 +1000

    updates to generate and finetune

--- llama2_finetune.py
+++ llama2_finetune.py
@@ -1,50 +1,65 @@
-from dataclasses import dataclass, field
 from typing import Optional, List
 import fire
-
+import sys
+import os
+import numpy as np
 
 import torch
 from datasets import load_dataset
 from peft import LoraConfig
-from tqdm import tqdm
-from transformers import AutoModelForCausalLM, BitsAndBytesConfig, HfArgumentParser, TrainingArguments
+from transformers import (
+    AutoModelForCausalLM, 
+    AutoTokenizer, 
+    BitsAndBytesConfig, 
+    TrainingArguments, 
+    DataCollatorForSeq2Seq, 
+    Trainer
+)
+import evaluate
+import pandas as pd
 
-from trl import SFTTrainer
+# from trl import SFTTrainer
+# from tqdm import tqdm
+# from dataclasses import dataclass, field
 
-tqdm.pandas()
+from peft import (
+    LoraConfig,
+    get_peft_model,
+    get_peft_model_state_dict,
+    prepare_model_for_int8_training,
+    set_peft_model_state_dict,
+)
+
 from utils.prompter import Prompter
 
-
-def generate_prompt(data_point, prompt_template_name):
-    prompter = Prompter(prompt_template_name)
-    full_prompt = prompter.generate_prompt(
-        data_point["instruction"],
-        data_point["input"],
-        data_point["output"],
-    )
-    return full_prompt
-
-
-
 def train(
-    # model/data params
-    base_model: str = "decapoda-research/llama-7b-hf", 
+    # data params
+    base_model: str = "meta-llama/Llama-2-7b-hf",
     dataset_name: str = "unwilledset/raven-data",
-    dataset_subset: str = "dataset-4",
+    dataset_subset: str = "dataset-2",
     dataset_split: str = "train",
-    dataset_text_field: str = "text",
     output_dir: str = "weights",
+    logging_dir: str = "logs",
     
-    # training hyperparams
-    batch_size: int = 128,
-    micro_batch_size: int = 8,
-    num_epochs: int = 10,
-    learning_rate: float = 1.41e-5,
-    cutoff_len: int = 512,
-    val_set_size: int = 2000,
+    # training/ model hyperparams
+    per_device_train_batch_size: int = 4,
+    per_device_eval_batch_size: int = 4,
     gradient_accumulation_steps: int = 16,
+    
+    # batch_size: int = 64,
+    # micro_batch_size: int = 4,
+    num_train_epochs: int = 3,
+    learning_rate: float = 1.41e-5,
+    val_set_size: int = 0.1, # 10%
     logging_steps: int = 1,
-    seq_length: int = 512,
+    optim: str = "adamw_torch",
+    eval_steps: int = 100, 
+    save_steps: int = 100,
+    warmup_steps: int = 100,
+    fp16: bool = True,
+    group_by_length: bool = False,  # faster, but produces an odd training loss curve
+    load_in_8bit: bool = True,
+    load_in_4bit: bool = False,
 
     # lora hyperparams
     use_peft: bool = True,
@@ -58,66 +73,88 @@ def train(
         "o_proj",
     ],
     
-    # llm hyperparams
+    # tokenizer settiongs
     train_on_inputs: bool = True,  # if False, masks out inputs in loss
-    add_eos_token: bool = False,
-    group_by_length: bool = False,  # faster, but produces an odd training loss curve
+    add_eos_token: bool = True,
+    max_length: int = 1024,
     
-    load_in_8bit: bool = True,
-    load_in_4bit: bool = False,
-
-    # wandb params
-    wandb_project: str = "",
-    wandb_run_name: str = "",
-    wandb_watch: str = "",  # options: false | gradients | all
-    wandb_log_model: str = "",  # options: false | true
+    
     resume_from_checkpoint: str = None,  # either training checkpoint or final adapter
-    prompt_template_name: str = "alpaca",  # The prompt template to use, will default to alpaca.
+    prompt_template_name: str = "alpaca_short",  # The prompt template to use, will default to alpaca.
 ):
     print(
-        f"Training Alpaca-LoRA model with params:\n"
-        f"base_model: {base_model}\n"
-        f"data_path: {dataset_name}\n"
-        f"dataset_subset: {dataset_subset}\n"
-        f"dataset_split: {dataset_split}\n"
-        f"output_dir: {output_dir}\n"
-        f"batch_size: {batch_size}\n"
-        f"micro_batch_size: {micro_batch_size}\n"
-        f"num_epochs: {num_epochs}\n"
-        f"learning_rate: {learning_rate}\n"
-        f"cutoff_len: {cutoff_len}\n"
-        f"val_set_size: {val_set_size}\n"
-        f"lora_r: {peft_lora_r}\n"
-        f"lora_alpha: {peft_lora_alpha}\n"
-        f"lora_dropout: {peft_lora_dropout}\n"
-        f"lora_target_modules: {peft_lora_target_modules}\n"
-        f"train_on_inputs: {train_on_inputs}\n"
-        f"add_eos_token: {add_eos_token}\n"
-        f"group_by_length: {group_by_length}\n"
-        f"load_in_8bit: {load_in_8bit}\n"
-        f"load_in_4bit: {load_in_4bit}\n"
+        f"Training model with params:\n\n"
+        f"Data params:\n"
+        f"   base_model: {base_model}\n"
+        f"   data_path: {dataset_name}\n"
+        f"   dataset_subset: {dataset_subset}\n"
+        f"   dataset_split: {dataset_split}\n"
+        f"   resume_from_checkpoint: {resume_from_checkpoint or False}\n"
+        f"   prompt template: {prompt_template_name}\n\n"
+        
+        f"Training hyperparams:\n"
+        f"   per_device_train_batch_size: {per_device_train_batch_size}\n"
+        f"   per_device_eval_batch_size: {per_device_eval_batch_size}\n"
+        f"   gradient_accumulation_steps: {gradient_accumulation_steps}\n"
+        f"   num_epochs: {num_train_epochs}\n"
+        f"   learning_rate: {learning_rate}\n"
+        f"   val_set_size: {val_set_size}\n"
+        f"   group_by_length: {group_by_length}\n"
+        f"   optim: {optim}\n"
+        f"   eval_steps: {eval_steps}\n"
+        f"   save_steps: {save_steps}\n"
+        f"   fp16: {fp16}\n"
+        f"   warmup_steps: {warmup_steps}\n"
+        f"   load_in_8bit: {load_in_8bit}\n"
+        f"   load_in_4bit: {load_in_4bit}\n\n"
+        f"   output_dir: {output_dir}\n"
+        f"   logging_dir: {logging_dir}\n\n"
+        
+        f"Tokenizer settings:\n"
+        f"   max_length: {max_length}\n"
+        f"   train_on_inputs: {train_on_inputs}\n"
+        f"   add_eos_token: {add_eos_token}\n\n"
 
-        f"wandb_project: {wandb_project}\n"
-        f"wandb_run_name: {wandb_run_name}\n"
-        f"wandb_watch: {wandb_watch}\n"
-        f"wandb_log_model: {wandb_log_model}\n"
-        f"resume_from_checkpoint: {resume_from_checkpoint or False}\n"
-        f"prompt template: {prompt_template_name}\n"
+        f"PEFT hyperparams:\n"        
+        f"   lora_r: {peft_lora_r}\n"
+        f"   lora_alpha: {peft_lora_alpha}\n"
+        f"   lora_dropout: {peft_lora_dropout}\n"
+        f"   lora_target_modules: {peft_lora_target_modules}\n\n"
+             
+
     )
 
+    # initialise the prompter
+    prompter = Prompter(prompt_template_name)
 
-    # Step 2: Load the dataset
-    dataset = load_dataset(
-        path=dataset_name, 
-        name=dataset_subset,        
-        split=dataset_split
-    )
-    train_data = dataset.shuffle().map(
-        generate_prompt, 
-        prompt_template_name
-    )
+    # function to tokenise the prompt
+    def tokenize(prompt):
+        result = tokenizer(
+            prompt,
+            truncation=True,
+            max_length=max_length,
+            padding=False,
+            return_tensors=None,
+        )
 
-    # Step 1: Load the model
+        # labels and input ids are the same
+        result["labels"] = result["input_ids"].copy()
+
+        return result
+
+    # function to generate and tokenise the prompt    
+    def generate_and_tokenize_prompt(data_point):
+        
+        # generate the prompt using the selected template
+        full_prompt = prompter.generate_prompt(
+            data_point["instruction"],
+            data_point["input"],
+            data_point["output"],
+        )
+
+        # tokenize and return    
+        return tokenize(full_prompt)
+   
     if load_in_8bit and load_in_4bit:
         raise ValueError("You can't load the model in 8 bits and 4 bits at the same time")
     elif load_in_8bit or load_in_4bit:
@@ -140,43 +177,125 @@ def train(
         torch_dtype=torch_dtype,
         use_auth_token=True,
     )
+    model = prepare_model_for_int8_training(model)
 
-
-
-    
-
-    training_args = TrainingArguments(
-        output_dir=output_dir,
-        per_device_train_batch_size=batch_size,
-        gradient_accumulation_steps=gradient_accumulation_steps,
-        learning_rate=learning_rate,
-        logging_steps=logging_steps,
-    )
-
-
-    # Step 4: Define the LoraConfig
+    # define the lora config
     if use_peft:
         peft_config = LoraConfig(
             r=peft_lora_r,
             lora_alpha=peft_lora_alpha,
+            lora_dropout=peft_lora_dropout,
+            target_modules=peft_lora_target_modules,
             bias="none",
             task_type="CAUSAL_LM",
         )
     else:
         peft_config = None
+    
+    # wrap the model in a peft model
+    model = get_peft_model(model, peft_config)
+
+    
+    # Load the dataset
+    dataset = load_dataset(
+        path=dataset_name, 
+        name=dataset_subset,        
+        split=dataset_split
+    )
+    
+    model.print_trainable_parameters()  
+    
+    # get the tokeniser from the model
+    tokenizer = AutoTokenizer.from_pretrained(model.config._name_or_path)
+    
+    tokenizer.pad_token_id = 0  # unk. we want this to be different from the eos token
+    tokenizer.padding_side = "left"  # Allow batched inference
+    tokenizer.add_eos_token = add_eos_token
+
+    if val_set_size > 0:        
+        if val_set_size < 1:
+            val_set_size = int(dataset.num_rows * val_set_size)
+
+        # split the dataset into training and validation
+        train_val = dataset.train_test_split(
+            test_size=val_set_size, shuffle=True, seed=42
+        )
 
 
-    # Step 5: Define the Trainer
-    trainer = SFTTrainer(
-        model=model,
-        args=training_args,
-        train_dataset=train_data,
-        dataset_text_field=dataset_text_field,
-        peft_config=peft_config,
+        train_data = (
+            train_val["train"].shuffle().map(generate_and_tokenize_prompt)
+        )
+        val_data = (
+            train_val["test"].shuffle().map(generate_and_tokenize_prompt)
+        )
+    else:
+        train_data = dataset["train"].shuffle().map(generate_and_tokenize_prompt)
+        val_data = None
+
+    training_args = TrainingArguments(
+        per_device_train_batch_size=per_device_train_batch_size,
+        per_device_eval_batch_size=per_device_eval_batch_size,
+        gradient_accumulation_steps=gradient_accumulation_steps,
+        num_train_epochs=num_train_epochs,
+        warmup_steps=warmup_steps,
+        learning_rate=learning_rate,
+        fp16=fp16,
+        logging_steps=logging_steps,
+        optim=optim,
+        evaluation_strategy="steps" if val_set_size > 0 else "no",
+        save_strategy="steps",
+        eval_steps=eval_steps if val_set_size > 0 else None,
+        save_steps=save_steps,
+        output_dir=output_dir,
+        logging_dir=logging_dir,
+        save_total_limit=3,
+        load_best_model_at_end=True if val_set_size > 0 else False,
+        group_by_length=group_by_length,
     )
 
-    trainer.train()
+
+    trainer = Trainer(
+        # model
+        model=model,
+        
+        # dataset
+        train_dataset=train_data,
+        eval_dataset=val_data,
+        
+        # args
+        args=training_args,
+        
+        # evaluation metric
+        # compute_metrics=compute_metrics,
+
+        # data collator
+        data_collator=DataCollatorForSeq2Seq(
+            tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True
+        ),
+    )
     
+    model.config.use_cache = False
+
+    old_state_dict = model.state_dict
+    model.state_dict = (
+        lambda self, *_, **__: get_peft_model_state_dict(
+            self, old_state_dict()
+        )
+    ).__get__(model, type(model))
+    
+    model = torch.compile(model)
+
+    train_result = trainer.train()
+    
+    model.save_pretrained(output_dir)
+
+    # save train results
+    # metrics = train_result.metrics
+    # trainer.log_metrics("train", metrics)
+    # trainer.save_metrics("train", metrics)
+
+    log_history = pd.DataFrame(trainer.state.log_history)
+    log_history.to_csv(os.path.join(logging_dir, "logs.csv"))
 
 
 if __name__ == "__main__":
---
+++ llama2_generate_interface.py
@@ -0,0 +1,268 @@
+import os
+import sys
+
+import fire
+import gradio as gr
+import torch
+import transformers
+from peft import PeftModel
+from transformers import (
+    GenerationConfig, 
+    AutoModelForCausalLM, 
+    AutoTokenizer,
+    BitsAndBytesConfig, 
+) 
+
+from utils.callbacks import Iteratorize, Stream
+from utils.prompter import Prompter
+
+if torch.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
+
+try:
+    if torch.backends.mps.is_available():
+        device = "mps"
+except:  # noqa: E722
+    pass
+
+def main(
+    load_8bit: bool = True,
+    base_model: str = "meta-llama/Llama-2-7b-hf",
+    lora_weights: str = "unwilledset/raven-model",
+    lora_weights_version: str = "",
+    prompt_template: str = "alpaca_short",  # The prompt template to use, will default to alpaca.
+    server_name: str = "0.0.0.0",  # Allows to listen on all interfaces by providing '0.
+    share_gradio: bool = False,
+    device_map: str = "auto",
+    load_in_8bit: bool = True,
+    load_in_4bit: bool = False,
+    use_peft: bool = True,
+):
+    base_model = base_model or os.environ.get("BASE_MODEL", "")
+    assert (
+        base_model
+    ), "Please specify a --base_model, e.g. --base_model='huggyllama/llama-7b'"
+
+
+    if load_in_8bit and load_in_4bit:
+        raise ValueError("You can't load the model in 8 bits and 4 bits at the same time")
+    elif load_in_8bit or load_in_4bit:
+        quantization_config = BitsAndBytesConfig(
+            load_in_8bit=load_in_8bit, load_in_4bit=load_in_4bit
+        )
+        # This means: fit the entire model on the GPU:0
+        device_map = {"": 0}
+        torch_dtype = torch.bfloat16
+    else:
+        device_map = None
+        quantization_config = None
+        torch_dtype = None
+
+    prompter = Prompter(prompt_template)
+    tokenizer = AutoTokenizer.from_pretrained(base_model)
+    
+    model = AutoModelForCausalLM.from_pretrained(
+        base_model,
+        quantization_config=quantization_config,
+        device_map=device_map,
+        trust_remote_code=True,
+        torch_dtype=torch_dtype,
+        use_auth_token=True,
+    )   
+
+    if use_peft:
+        model = PeftModel.from_pretrained(
+            model,
+            lora_weights,
+            torch_dtype=torch.float16,
+            device_map=device_map,
+            force_download=True,
+        )
+   
+    # unwind broken decapoda-research config
+    model.config.pad_token_id = tokenizer.pad_token_id = 0  # unk
+    model.config.bos_token_id = 1
+    model.config.eos_token_id = 2
+
+    if not load_8bit:
+        model.half()  # seems to fix bugs for some users.
+
+    model.eval()
+    model = torch.compile(model)
+
+    def evaluate(
+        instruction,
+        input=None,
+        temperature=0.1,
+        top_p=0.75,
+        top_k=40,
+        num_beams=4,
+        max_new_tokens=128,
+        stream_output=False,
+        **kwargs,
+    ):
+        prompt = prompter.generate_prompt(instruction, input)
+        inputs = tokenizer(prompt, return_tensors="pt")
+        input_ids = inputs["input_ids"].to(device)
+        generation_config = GenerationConfig(
+            temperature=temperature,
+            top_p=top_p,
+            top_k=top_k,
+            num_beams=num_beams,
+            **kwargs,
+        )
+
+        generate_params = {
+            "input_ids": input_ids,
+            "generation_config": generation_config,
+            "return_dict_in_generate": True,
+            "output_scores": True,
+            "max_new_tokens": max_new_tokens,
+        }
+
+        if stream_output:
+            # Stream the reply 1 token at a time.
+            # This is based on the trick of using 'stopping_criteria' to create an iterator,
+            # from https://github.com/oobabooga/text-generation-webui/blob/ad37f396fc8bcbab90e11ecf17c56c97bfbd4a9c/modules/text_generation.py#L216-L243.
+
+            def generate_with_callback(callback=None, **kwargs):
+                kwargs.setdefault(
+                    "stopping_criteria", transformers.StoppingCriteriaList()
+                )
+                kwargs["stopping_criteria"].append(
+                    Stream(callback_func=callback)
+                )
+                with torch.no_grad():
+                    model.generate(**kwargs)
+
+            def generate_with_streaming(**kwargs):
+                return Iteratorize(
+                    generate_with_callback, kwargs, callback=None
+                )
+
+            with generate_with_streaming(**generate_params) as generator:
+                for output in generator:
+                    # new_tokens = len(output) - len(input_ids[0])
+                    decoded_output = tokenizer.decode(output)
+
+                    if output[-1] in [tokenizer.eos_token_id]:
+                        break
+
+                    yield prompter.get_response(decoded_output)
+            return  # early return for stream_output
+
+        # Without streaming
+        with torch.no_grad():
+            generation_output = model.generate(
+                input_ids=input_ids,
+                generation_config=generation_config,
+                return_dict_in_generate=True,
+                output_scores=True,
+                max_new_tokens=max_new_tokens,
+            )
+        s = generation_output.sequences[0]
+        output = tokenizer.decode(s)
+        yield prompter.get_response(output)
+
+
+    demo = gr.Blocks(
+        title="Raven", 
+        analytics_enabled=True
+    )
+    
+    callback = gr.CSVLogger()
+
+    with demo:    
+        gr.Markdown(
+        """
+            <p style="text-align:center; font-size:3em;">            
+                📈 Raven 📈        
+            </p>
+        """
+        )
+        gr.Markdown(
+            """
+            <p style="text-align: center;">
+                Raven is a 7B-parameter <a href="https://ai.meta.com/llama/">LLaMA 2 model</a> finetuned to follow instructions in the finance domain. <br/>
+                It is trained on the <a href="https://github.com/tatsu-lab/stanford_alpaca">Stanford Alpaca</a> dataset and several <a href="https://huggingface.co/datasets/unwilledset/raven-data/viewer/dataset-2/train">other finance datasets</a>. For more information, please visit <a href="https://github.com/adriantheuma/fin-expert">the project's website</a>.  
+            </p>
+            """
+        )
+        with gr.Row():   
+            with gr.Column(scale=50):     
+                with gr.Row(variant="panel"):                     
+                    instruction = gr.components.Textbox(
+                        lines=2,
+                        label="Instruction",                
+                        placeholder="How is a raven related to wealth?",
+                    )
+                with gr.Row(variant="panel"):     
+                    input = gr.components.Textbox(
+                        lines=2, 
+                        label="Input", 
+                        info="Provide further context such as a passage.",
+                        placeholder="none"
+                    )   
+                with gr.Row(variant="panel"):    
+                    temperature = gr.components.Slider(
+                        minimum=0, maximum=1, value=0.1, label="Temperature", info="A higher temperature value typically makes the output more diverse and creative but might also increase its likelihood of straying from the context."
+                    )
+                    top_k = gr.components.Slider(
+                        minimum=0, maximum=100, step=1, value=10, label="Top-k", info="Top-k tells the model to pick the next token from the top ‘k’ tokens in its list, sorted by probability."
+                    )
+                    top_p = gr.components.Slider(
+                        minimum=0, maximum=1, value=0.75, label="Top-p", info="Top-p picks from the top-k tokens based on the sum of their probabilities."
+                    )   
+                with gr.Row(variant="panel"):
+                    beams = gr.components.Slider(
+                        minimum=1, maximum=4, step=1, value=2, label="Beams", info="The final output is the most probable sequence of tokens found among the k beams."
+                    )
+                    max_tokens = gr.components.Slider(
+                        minimum=1, maximum=2000, step=1, value=256, label="Max tokens", info="Stopping criteria for the model"
+                    )
+                    stream = gr.components.Checkbox(
+                        label="Stream output", value=True, info="Stream the output one token at a time."
+                    )   
+                with gr.Row():
+                    inputs = [temperature, top_k, top_p, beams, max_tokens, stream]
+                    clear_btn = gr.ClearButton(value="Clear")
+                    submit_btn = gr.components.Button(value="Submit", variant="primary")
+                    
+            with gr.Column(scale=50, variant="panel"):
+                output = gr.inputs.Textbox(
+                    lines=10,
+                    label="Output",
+                )
+                flag_btn = gr.Button("Flag this output", variant="stop")
+        
+        # list of inputs and outputs
+        inputs = [instruction, input, temperature, top_k, top_p, beams, max_tokens, stream]
+        outputs = [output]
+        
+        # clear button setup
+        components_to_clear = [instruction, input, output]
+        clear_btn.add(components=components_to_clear)
+
+        # submit on click 
+        submit_btn.click(fn=evaluate, inputs=inputs, outputs=outputs)
+
+        # We can choose which components to flag -- in this case, we'll flag all of them
+        callback.setup(inputs + outputs, "flagged_data_points")
+        flag_btn.click(
+            fn=lambda *args: callback.flag(args), 
+            inputs=inputs + outputs, 
+            outputs=None, 
+            preprocess=False,
+            show_progress="full"
+        )
+        
+
+    demo.queue().launch(
+        server_name="0.0.0.0", 
+        auth=("user", "HJ49AJnXy36kKYTg")
+    )
+
+if __name__ == "__main__":
+    fire.Fire(main)
